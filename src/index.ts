72a7537c95a534c539b031c5c9dccd7301de014f
SzeLong <szelong@shoplineapp.com>
2021-08-04 15:04:29 +0800

    feat: spellcheck with position

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,57 @@
 // Provides dev-time type structures for  `danger` - doesn't affect runtime.
-import { DangerDSLType } from "../node_modules/danger/distribution/dsl/DangerDSL";
-declare var danger: DangerDSLType;
-export declare function message(message: string): void;
-export declare function warn(message: string): void;
-export declare function fail(message: string): void;
-export declare function markdown(message: string): void;
+import { DangerDSLType } from "../node_modules/danger/distribution/dsl/DangerDSL"
+declare var danger: DangerDSLType
+export declare function message(message: string): void
+export declare function warn(message: string): void
+export declare function fail(message: string): void
+export declare function markdown(message: string): void
+
+import * as cspell from "cspell-lib"
+import { extname } from "path"
+
+interface FileTypo {
+  file: string
+  infoItems: cspell.TextInfoItem[]
+}
+
+async function checkFile(file: string): Promise<FileTypo> {
+  const diff = await danger.git.diffForFile(file)
+  if (!diff) {
+    return { file, infoItems: [] }
+  }
+  const source = diff.after
+
+  const ext = extname(file)
+  const languageIds = cspell.getLanguagesForExt(ext)
+  const mergedSettings = cspell.mergeSettings(cspell.getDefaultSettings(), {
+    source: { name: file, filename: file },
+  })
+  const config = cspell.constructSettingsForText(mergedSettings, source, languageIds)
+
+  return {
+    file,
+    infoItems: await cspell.checkText(source, config).then(info => {
+      return info.items.filter(i => i.isError)
+    }),
+  }
+}
+
+function describeTypo(typo: FileTypo): string {
+  return (
+    `Typos in ${typo.file}\n|Position|Word|\n|-|-|\n` +
+    typo.infoItems.map(infoItem => `|${infoItem.startPos}|${infoItem.text}|`).join("\n")
+  )
+}
 
 /**
  * This plugin checks the spelling in code and reports any error as markdown PR comment.
  */
-export default function gitSpellcheck() {
-  // Replace this with the code from your Dangerfile
-  const title = danger.github.pr.title;
-  message(`PR Title: ${title}`);
+export async function gitSpellcheck() {
+  const files = [...danger.git.modified_files, ...danger.git.created_files]
+  const typos = await Promise.all(files.map(checkFile))
+  const msg = typos
+    .filter(typo => typo.infoItems.length > 0)
+    .map(describeTypo)
+    .join("\n")
+  warn(msg)
 }
