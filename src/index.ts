7b0ee81efb01d816d7ff1ba245d0e82b98c90fd8
SzeLong <szelong@shoplineapp.com>
2021-08-05 12:16:40 +0800

    fix: syntax

--- src/index.ts
+++ src/index.ts
@@ -67,7 +67,7 @@ async function checkFile(file: string, settings: cspell.FileSettings): Promise<F
 
 function describeInfo(info: FileInfo): string {
   return (
-    `Typos in ${info.file}\n| Position (Line:Col) | Word |\n| --- | --- |\n` +
+    `### Typos in ${info.file}\n\n| Position (Line:Col) | Word |\n| --- | --- |\n` +
     info.typos.map(typo => `| ${typo.line}:${typo.col} | ${typo.text} |`).join("\n")
   )
 }
